815363a2fde0a3742abe8d5a8a63c242901c40bb
Martin Karry <martin@karry.de>
2024-09-15 15:55:42 +0200

    Added imprint

--- packages/swim/src/app/(web)/anmelden/layout.tsx
+++ packages/swim/src/app/(web)/anmelden/layout.tsx
@@ -1,5 +1,10 @@
 export default function RegisterLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
-  return <div className="bg-white/25 p-4 rounded-lg backdrop-blur xl:mx-64 lg:mx-32 md:mx-16 mx-8 mt-4 mb-8">
-    {children}
+  return <div>
+    <div className="bg-white/25 p-4 rounded-lg backdrop-blur xl:mx-64 lg:mx-32 md:mx-16 mx-8 mt-4 mb-8">
+      {children}
+    </div>
+    <div className="bg-white/25 p-4 rounded-lg backdrop-blur xl:mx-64 lg:mx-32 md:mx-16 mx-8 mt-4 mb-8">
+      <a href="https://giessen.dlrg.de/impressum-und-datenschutz/">Impressum</a>
+    </div>
   </div>
 }
