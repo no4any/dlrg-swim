575672689b4a253490007d88e93066e85e4d3cc8
Martin Karry <martin@karry.de>
2024-09-15 18:19:25 +0200

    added export

--- packages/swim/src/app/(web)/page.tsx
+++ packages/swim/src/app/(web)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
+import getSwimmersCollection from "@/lib/mongo/getSwimmersCollection"
+import getTeamsCollection from "@/lib/mongo/getTeamsCollection";
+import fs from "fs";
+
+const TEAMS_FILE = "/data/teams.json";
+const SWIMMERS_FILE = "/data/swimmers.json";
+
+export default async function Home() {
+  const scol = await getSwimmersCollection();
+  const sJson= JSON.stringify(scol.find({}).toArray());
+  fs.writeFileSync(SWIMMERS_FILE, sJson);
+  const tcol = await getTeamsCollection();
+  const tJson = JSON.stringify(tcol.find({}).toArray());
+  fs.writeFileSync(TEAMS_FILE, tJson);
+
   return <div>Hello DLRG!</div>
 }
