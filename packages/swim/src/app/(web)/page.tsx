ec095b0835cb48ba0e89645ec6e3475704d840d3
Martin Karry <martin@karry.de>
2024-09-15 18:38:51 +0200

    Typo

--- packages/swim/src/app/(web)/page.tsx
+++ packages/swim/src/app/(web)/page.tsx
@@ -9,11 +9,13 @@ export const revalidate = 0;
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
+  console.log("Exporting swimmers")
   const scol = await getSwimmersCollection();
-  const sJson= JSON.stringify(scol.find({}).toArray());
+  const sJson= JSON.stringify(await scol.find({}).toArray());
   fs.writeFileSync(SWIMMERS_FILE, sJson);
+  console.log("Exporting teams")
   const tcol = await getTeamsCollection();
-  const tJson = JSON.stringify(tcol.find({}).toArray());
+  const tJson = JSON.stringify(await tcol.find({}).toArray());
   fs.writeFileSync(TEAMS_FILE, tJson);
 
   return <div>Hello DLRG!</div>
