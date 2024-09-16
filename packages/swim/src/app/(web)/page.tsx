a116e43455b3e04e2e4f48164276b3af9c40967d
Martin Karry <martin@karry.de>
2024-09-16 16:22:18 +0200

    Added try to make usable anyway

--- packages/swim/src/app/(web)/page.tsx
+++ packages/swim/src/app/(web)/page.tsx
@@ -9,14 +9,17 @@ export const revalidate = 0;
 export const dynamic = 'force-dynamic';
 
 export default async function Home() {
-  console.log("Exporting swimmers")
-  const scol = await getSwimmersCollection();
-  const sJson= JSON.stringify(await scol.find({}).toArray());
-  fs.writeFileSync(SWIMMERS_FILE, sJson);
-  console.log("Exporting teams")
-  const tcol = await getTeamsCollection();
-  const tJson = JSON.stringify(await tcol.find({}).toArray());
-  fs.writeFileSync(TEAMS_FILE, tJson);
+  try {
+
+    console.log("Exporting swimmers")
+    const scol = await getSwimmersCollection();
+    const sJson = JSON.stringify(await scol.find({}).toArray());
+    fs.writeFileSync(SWIMMERS_FILE, sJson);
+    console.log("Exporting teams")
+    const tcol = await getTeamsCollection();
+    const tJson = JSON.stringify(await tcol.find({}).toArray());
+    fs.writeFileSync(TEAMS_FILE, tJson);
+  } catch (e) { }
 
   return <div>Hello DLRG!</div>
 }
