4c99f1a08f750909a0c8e07bf1c652c40ee0c74f
Martin Karry <martin@karry.de>
2024-09-17 13:32:17 +0200

    Fixed Problem with multiple MongoClients

--- packages/swim/src/lib/mongo/getMongoClient.ts
+++ packages/swim/src/lib/mongo/getMongoClient.ts
@@ -1,19 +1,18 @@
 import { MongoClient } from "mongodb";
-import { MONGO_CONNECTION, MONGO_PASSWORD, MONGO_USERNAME,  } from "../params";
+import { MONGO_CONNECTION, MONGO_PASSWORD, MONGO_USERNAME, } from "../params";
 
-export const mongo: MongoClient | undefined = undefined;
+var mongo: MongoClient | undefined;
 
 export default async function getMongoClient(): Promise<MongoClient> {
-    if (mongo) {
-        return mongo;
+    if (mongo === undefined) {
+        console.log("Creating MongoClient!")
+        mongo = new MongoClient(MONGO_CONNECTION, {
+            auth: {
+                username: MONGO_USERNAME,
+                password: MONGO_PASSWORD
+            }
+        })
     }
 
-    console.log(`Connection to ${MONGO_CONNECTION}`)
-    
-    return new MongoClient(MONGO_CONNECTION, {
-        auth: {
-            username: MONGO_USERNAME,
-            password: MONGO_PASSWORD
-        }
-    })
+    return mongo;
 }
