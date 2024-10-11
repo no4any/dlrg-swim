6b37c4c08df1592e37236a92bfb1571081e8f05b
Martin Karry <martin@karry.de>
2024-10-11 15:46:37 +0200

    Precice warning

--- packages/swim/src/app/(web)/admin/swimmer/[id]/register/RegisterForm.tsx
+++ packages/swim/src/app/(web)/admin/swimmer/[id]/register/RegisterForm.tsx
@@ -22,7 +22,7 @@ export default function RegisterForm({ swimmer }: { swimmer: Swimmer }) {
         <div>{swimmer.email}</div>
         <H2>Registrierung</H2>
         {swimmer.birthday ? <div className="block bg-dlrg-yellow rounded p-2 my-2"><b>WARNUNG: </b>KEINE Teilnahme an Alterswertung</div> : <></>}
-        {!swimmer.gender ||  swimmer.gender === "0"? <div className="block bg-dlrg-yellow rounded p-2 my-2"><b>WARNUNG: </b>Einzelwertung NICHT möglich</div> : <></>}
+        {!swimmer.gender ||  swimmer.gender === "0"? <div className="block bg-dlrg-yellow rounded p-2 my-2"><b>WARNUNG: </b>Einzelwertung NICHT möglich (angabe Geschlecht fehlt)</div> : <></>}
         <form action={formAction}>
             <input name="id" type="hidden" value={swimmer._id?.toString() || ""} />
             <div className="pb-4">
